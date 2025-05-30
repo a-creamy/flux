aa1b2900cc740c678c53a8b224c6e7f7e019f90e
a-creamy <akramo@tuta.io>
2025-05-30 15:20:08 +0000

    Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import './App.css'
 import Number from './components/number'
 
 function App() {
-
     return (
         <>
             {Number()}
--- src/components/number.tsx
+++ src/components/number.tsx
@@ -3,10 +3,13 @@ import '../App.css';
 
 export default function Number() {
     const [count, setCount] = createSignal(0);
+    const [clickRate] = createSignal(1);
 
     return (
-        <button class="number" onClick={() => {
-            setCount((count) => count + 1)
-        }}>{count()}</button>
-    )
+        <>
+            <button class="number" onClick={() => {
+                setCount((count) => count + clickRate());
+            }}>{count()}</button>
+        </>
+    );
 }
