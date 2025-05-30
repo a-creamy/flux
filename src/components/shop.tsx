839a5b5b26c64d499fec6e8732e50552d382d10e
a-creamy <akramo@tuta.io>
2025-05-30 16:35:04 +0000

    Shop skeleton

--- src/components/shop.tsx
+++ src/components/shop.tsx
@@ -24,6 +24,27 @@ export default function Shop() {
     });
 
     return (
-        <div class='shop'></div>
+        <div class='shop'>
+            <div style={{
+                "z-index": 10,
+                position: 'absolute',
+                top: '5vh',
+                left: '50%',
+                transform: 'translateX(-50%)',
+                "font-size": '4rem',
+                padding: '10px',
+                border: '3px solid black',
+            }}>Shop</div>
+            <div style={{
+                "z-index": 10,
+                position: 'absolute',
+                right: '5vw',
+                top: '50%',
+                transform: 'translateY(-50%)',
+                height: '90vh',
+                width: '30vw',
+                border: '2px solid black',
+            }}></div>
+        </div>
     )
 }
