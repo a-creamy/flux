b6a36675f330dba5f78802b5f31c64d00e6926e0
a-creamy <akramo@tuta.io>
2025-06-04 18:43:08 +0000

    Good ui

--- src/components/shop.tsx
+++ src/components/shop.tsx
@@ -1,5 +1,6 @@
 import { For } from "solid-js";
-import { game, setGame } from "./game"
+import { game, setGame } from "./game";
+import "../App.css";
 
 interface ProducerProps {
     index: number,
@@ -10,32 +11,35 @@ const Producer = (props: ProducerProps) => {
     const producers = game.shop.producers;
 
     return (
-        <div onClick={() => {
-            if (game.time >= producers[index].price) {
-                setGame("time", () => game.time - producers[index].price);
-                setGame("shop", "producers", index, () => ({
-                    ...producers[index],
-                    price: producers[index].price * 1.15,
-                    amount: producers[index].amount + 1,
-                }));
-            }
-        }}>
-            <div>{producers[index].name}</div>
-            <div>{producers[index].price}</div>
-            <div>{producers[index].amount}</div>
+        <div
+            onClick={() => {
+                if (game.time >= producers[index].price) {
+                    setGame("time", () => game.time - producers[index].price);
+                    setGame("shop", "producers", index, () => ({
+                        ...producers[index],
+                        price: producers[index].price * 1.15,
+                        amount: producers[index].amount + 1,
+                    }));
+                }
+            }}
+            class="producer"
+        >
+            <div class="producer__name">{producers[index].name}</div>
+            <div class="producer__price">{Math.floor(producers[index].price)}</div>
+            <div class="producer__amount">{producers[index].amount}</div>
         </div>
     )
 }
 
 const Shop = () => {
     return (
-        <>
+        <div class='shop'>
             <For each={game.shop.producers}>
                 {(_, index) =>
                     <Producer index={index()} />
                 }
             </For>
-        </>
+        </div>
     )
 }
 
